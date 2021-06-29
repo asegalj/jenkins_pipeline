0a3289d952022e7c08a725faeecbaae7a9bdc74d
asegalj <amir.segal13@gmail.com>
2021-06-29 21:12:26 +0300

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -10,13 +10,13 @@ pipeline {
 
     stage('Build') {
         steps {
+            echo "This is build number $BUILD_NUMBER of demo $DEMO"
+            sh '''
+                echo "Using a multi-line shell step"
+                chmod +x test.sh
+                ./test.sh
+              '''
             sh 'echo hello world'
-        }
-
-        post {
-            always {
-                sh 'echo test'
-            }
         }
     }
 
