151bc7dc44817b25404be9993d785938ffa37249
asegalj <amir.segal13@gmail.com>
2021-07-01 10:13:25 +0300

    Update Jenkinsfile2.groovy

--- Jenkinsfile2.groovy
+++ Jenkinsfile2.groovy
@@ -34,13 +34,13 @@ pipeline {
                    sh "git config  user.email 'devopstasks2021@gmail.com'"
                   sh  "git config  user.name 'devops2021devops'"
                   sh "git config  user.password 'Devops2021'"
-                  sh "git checkout -b Dev | git checkout Dev"
+                  sh "git checkout -b Dev || git checkout Dev"
                   sh 'echo "test" > result.json'
                   sh "ls"
                   sh "git add result.json"
                   sh "ls"
                   sh "git commit -m 'add result.json file to Dev Stage'"
-                  sh "git checkout -b Staging | git checkout Staging" 
+                  sh "git checkout -b Staging || git checkout Staging" 
               
                   //  git commit -m "add result.json file to ${env.RELEASE}" 
                   sh "git push https://github.com/asegalj/jenkins-git.git Dev Staging"
